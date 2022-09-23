39aaec68bf86787a89b4d8b96d9e425171630b70
DaniyalAhunov <daniyal.ahunov@icloud.com>
2022-09-23 11:14:52 +0300

    добавил семинар 6 все что нашел

---
+++ Seminar6/DZ3.py
@@ -0,0 +1,15 @@
+# Задайте список из N элементов, заполненных числами из промежутка [-N, N].
+# Найдите произведение элементов на указанных пользователем через пробел позициях.
+import random
+priz = 1
+N = int(input('Введите количество элементов: '))
+listN = [random.randint(N * -1, N) for i in range(N)]
+# for i in range(0, N):
+#     num = random.randint(N * -1, N)
+#     listN.append(num)
+print(f'Список готов \n{listN}')
+pozit = input('Введите место элементов через пробел(нумерация от 0): ') 
+for i in range(0, len(pozit.split(' '))):
+    a = int(pozit.split(' ')[i])
+    priz *= listN[a]
+print(f'Произведение выбранных позиций: {priz}')
